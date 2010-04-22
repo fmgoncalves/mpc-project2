5758ed6d9cbdc81ef22aac0e168229d9511d2e37
Filipe Gonçalves <fmgoncalves@users.noreply.github.com>
2010-04-22 14:12:44 +0000

    Initial commit. Contains code for wireless scan on linux environments.

---
+++ src/mpc/wifi/gui/ScanWindow.java
@@ -0,0 +1,26 @@
+package mpc.wifi.gui;
+
+import javax.swing.JFrame;
+
+
+public class ScanWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * TODO 
+	 * proposed layout:
+	 * - scan button to trigger scanning
+	 * - table with results
+	 * - list of locations + button to add (location, results)
+	 */
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/mpc/wifi/lib/linux/WiFiInfo.java
@@ -0,0 +1,94 @@
+package mpc.wifi.lib.linux;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+import mpc.wifi.lib.SignalStrength;
+
+
+
+public class WiFiInfo {
+	
+	/**
+	 * Change to "" to avoid giving super user privileges to this application.
+	 * Otherwise use "gksu".
+	 */
+	private static final String SU_PRIVILEGES = "gksu";
+	
+	private String wifi_interface = "ra0";
+
+	public WiFiInfo() {
+	}
+	
+	public WiFiInfo(String wifi_interface) {
+		this.wifi_interface = wifi_interface;
+	}
+	
+
+	public List<SignalStrength> scan() throws IOException, InterruptedException {
+		Process p = Runtime.getRuntime().exec(SU_PRIVILEGES +" "+ "iwlist "+ wifi_interface +" ap");
+		
+		InputStream is = p.getInputStream();
+
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		
+		int c;
+		while((c= is.read()) != -1)
+			bos.write(c);
+		
+		byte[] b = bos.toByteArray();
+		
+		return parseScanData(new String(b,"UTF-8"));
+		
+	}
+	
+	/**
+	 * The provided String should come in the format returned by
+	 * <code>iwlist \<interface\> scan</code> command.<br>
+	 * Example:<br>
+	 * <blockquote>
+	 * ra0       Peers/Access-Points in range:
+	 *     00:12:DA:AE:B2:A0 : Quality:96/100  Signal level:-52 dBm  Noise level:-81 dBm
+	 *     00:12:DA:9E:30:32 : Quality:5/100  Signal level:-88 dBm  Noise level:-81 dBm
+	 * </blockquote>
+	 * This function parses that string and returns an array of <tt>SignalStrength</tt>.
+	 * @param data
+	 * @return an array of <tt>SignalStrength</tt>
+	 */
+	private List<SignalStrength> parseScanData(String data){
+		List<SignalStrength> l = new LinkedList<SignalStrength>();
+		
+		//Divide by line breaks
+		String[] nonbl = data.split("\n");
+		
+		//Parse each line to obtain BSSID and the corresponding signal strength
+		for(int i = 1; i < nonbl.length; i++) {
+			String line = nonbl[i];
+			String[] rmws = line.split(" ");
+			
+			if(rmws.length <= 1) continue;
+			String bssid = rmws[4];
+			
+			String sigstr = rmws[9].split(":")[1];
+			
+			int signal = Integer.parseInt(sigstr);
+			
+			l.add(new SignalStrength(bssid, signal));
+			
+		}
+		
+		
+		return l;
+	}
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		WiFiInfo wi = new WiFiInfo();
+		
+		for(SignalStrength ss : wi.scan())
+			System.out.printf("Access Point %s\t%d\n", ss.getBssid(), ss.getSignal());
+	}
+
+}
